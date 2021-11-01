552e2b02e7e35cb48d16f620a0b30f3c37778864
rishi05051997 <vrushabhdhatral10@gmail.com>
2021-11-01 21:48:23 +0530

    added game

---
+++ Dom-Manipulation-7/Guess-my-no/starter/script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+
+// console.log(document.querySelector('.message').textContent)
+
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+
+
+
+// // console.log(document.querySelector('.guess').value)
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+console.log(secretNumber);
+let score = 20;
+
+
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess =Number(document.querySelector('.guess').value);
+    
+    console.log(typeof guess);
+    //// When there is no input
+    if(!guess){
+        document.querySelector('.message').textContent = '🚫 No Number!';
+
+
+    } 
+    //// when player wins
+    else if(guess === secretNumber){
+        
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+    } 
+    ///// when player input is more than secret number
+    else if(guess > secretNumber){
+        
+       
+        if(score > 1){
+            
+            document.querySelector('.message').textContent = '📈 Too High!';
+            score --;
+            document.querySelector('.score').textContent = score;
+            
+        } else {
+            document.querySelector('.message').textContent = '💣  You Lost the game!'; 
+            document.querySelector('.score').textContent = 0;
+        }
+       
+    } 
+    ///// when player input is less than secret number
+    else if(guess < secretNumber){
+        if(score > 1){
+            
+            document.querySelector('.message').textContent = '📉 Too low!';
+            score --;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💣 You Lost the game!'; 
+            document.querySelector('.score').textContent = 0;
+        }
+       
+        
+        
+    }
+    
+})
---
+++ Javascript-Fundamentals-1/starter/script.js
@@ -0,0 +1,109 @@
+// let js = 'amazing';
+// console.log( 40+20-11+55);
+
+// console.log("Vrushabh");
+// console.log(23)
+
+// let firstName = "Vrushabh";
+
+// console.log(firstName);
+
+// let PI = 3.14;
+
+// let myFirstJob = "Programmer";
+
+// let myCurrentJob = "Teacher";
+
+/////// Data Types 
+
+// let javsScriptIsFun = true;
+
+// console.log(javsScriptIsFun);
+
+// console.log(typeof(javsScriptIsFun));
+// console.log(typeof(23));
+// console.log(typeof("javsScriptIsFun"));
+
+// javsScriptIsFun = "YES!";
+// console.log(typeof(javsScriptIsFun));
+
+// let year;
+// console.log(typeof(year));
+
+// year = 1991;
+
+// console.log(year);
+// console.log(typeof(year));
+
+// console.log(typeof null);
+
+
+/// ***************** let, var, const *****************
+/// let and const are introduced in ES-6 
+/// var is old fashioned
+/// there is no any difference in between let and var but only in modern js we usualu used let instead of var
+
+
+// let age = 30;
+// age = 31;
+// console.log(age);
+
+
+// const birthYear = 1997;
+
+// // birthYear = 1998; ///// error Assignment to constant
+
+// const job;
+
+// var job = 'programmer';
+
+// job = 'teacher';
+
+
+///// Operators - tranform multiple values
+//// Arithmatic Operators
+
+const now = 2021
+
+const rishiAge = now - 1997;
+
+console.log(rishiAge);
+
+const ageJayesh = now - 1993;
+
+console.log(ageJayesh);
+
+console.log(rishiAge * 2)
+console.log(rishiAge / 2)
+
+const firstName = "Vrushabh";
+const lastNaame = "Dhatrak";
+
+console.log(`${firstName} ${lastNaame}`);
+
+
+/// Assignment Operators 
+
+
+let x = 10 + 5;
+
+x += 10;
+
+console.log(x);
+
+x *= 10;
+x++;
+x--;
+
+console.log(x);
+
+
+/// Comparison Operators\
+console.log(ageJayesh>rishiAge)
+
+
+
+
+
+
+
