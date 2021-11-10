735693b034af832b3f86c4258c1bebba0126e43d
rishi05051997 <vrushabhdhatral10@gmail.com>
2021-11-10 06:13:09 +0530

    modal project completed

---
+++ Modal-Project/starter/script.js
@@ -0,0 +1,32 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+
+
+const closedModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openedModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closedModalByEscapeKey = (e) => {
+    if(e.key === "Escape" && !modal.classList.contains('hidden')){
+        closedModal();
+    }
+}
+
+for(let i=0;i<btnsOpenModal.length;i++){
+    btnsOpenModal[i].addEventListener('click', openedModal);
+}
+
+closeModal.addEventListener('click', closedModal);
+
+overlay.addEventListener('click' , closedModal);
+
+document.addEventListener('keydown', closedModalByEscapeKey);
